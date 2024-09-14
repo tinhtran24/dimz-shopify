e769ec244792d45f014e3c8bf863de4fdaa9e153
Max Tran <max_tran@nfcguest.com>
2024-09-14 10:57:42 +0700

    init:project

---
+++ assets/ecom-66630f145f4f1629710689f7.js
@@ -0,0 +1,4 @@
+/* Publish by EComposer at 2024-06-07 14:24:37*/
+;try{
+ 
+} catch(error){console.error(error);}
---
+++ assets/ecom-667a917b64f1281ace0b2042.js
@@ -0,0 +1,23 @@
+/* Publish by EComposer at 2024-06-25 09:44:30*/
+                (function(){
+                    const Func = (function() {
+                        'use strict';
+window.__ectimmers = window.__ectimmers ||{};window.__ectimmers["ecom-c6jvdddd6sr"]=  window.__ectimmers["ecom-c6jvdddd6sr"] || {};
+if(this.settings.link==="lightbox"&&this.settings.lightbox==="yes"&&window.EComModal&&this.$el){var e=this.$el.querySelector("[ecom-modal]");new window.EComModal(e,{cssClass:["ecom-container-lightbox-"+this.id]})}let i=this.$el;if(!i)return;function t(s){const o=s.getBoundingClientRect();return o.top>=0&&o.left>=0&&o.bottom-s.offsetHeight/2<=(window.innerHeight||document.documentElement.clientHeight)&&o.right<=(window.innerWidth||document.documentElement.clientWidth)}function n(){let s=i.querySelector(".ecom-element.ecom-base-image"),o=i.closest(".core__row--columns");s&&(t(s)?(s.classList.add("image-highlight"),o.setAttribute("style","z-index: unset")):(s.classList.remove("image-highlight"),o.setAttribute("style","z-index: 1")))}this.settings.highligh_on_viewport&&window.addEventListener("scroll",function(){n()})
+
+                    });
+                    
+                        document.querySelectorAll('.ecom-c6jvdddd6sr').forEach(function(el){
+                            Func.call({$el: el, id: 'ecom-c6jvdddd6sr', settings: {"link":"none","lightbox":"no","highligh_on_viewport":false},isLive: true});
+                        });
+                    
+                        document.querySelectorAll('.ecom-slknxqn1n3').forEach(function(el){
+                            Func.call({$el: el, id: 'ecom-slknxqn1n3', settings: {"link":"none","lightbox":"no","highligh_on_viewport":false},isLive: true});
+                        });
+                    
+
+                })();
+            
+;try{
+ 
+} catch(error){console.error(error);}
